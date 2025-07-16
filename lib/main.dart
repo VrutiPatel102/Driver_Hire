4fc831a79e4a56be8445de51e8697a62b2a5a967
vruti <vrutipatel102@gmail.com>
2025-07-16 17:21:38 +0530

    driver(booking,profile)screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:driver_hire/navigation/appRoute.dart';
 import 'package:driver_hire/navigation/routeGenerator.dart';
-import 'package:driver_hire/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
